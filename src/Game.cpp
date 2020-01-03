bbe9898d77a703a7f17b6ed6064552ca4c213b86
David Mendoza <d.mendoza99@unitec.edu>
2020-01-02 23:51:02 -0600

    simplified that freaking weird gameloop

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,34 +3,28 @@
 
 namespace Skeleton {
 Game::Game(int width, int height, std::string title, int limit) {
-  _data->window.create(sf::VideoMode(width, height), title,);
+  _data->window.create(sf::VideoMode(width, height), title);
   _data->window.setFramerateLimit(limit);
   _data->machine.addState(StateRef(new SplashState(this->_data)));
   this->run();
 }
 
 void Game::run() {
-  float newTime, frametime, interpolation;
-  float currentTime = this->_clock.getElapsedTime().asSeconds();
+  float frametime;
   float accumulator = 0.0f;
   while (this->_data->window.isOpen()) {
     this->_data->machine.processStateChanges();
-    newTime = this->_clock.getElapsedTime().asSeconds();
-    frametime = newTime - currentTime;
 
-    if (frametime > 0.25f) {
-      frametime = 0.25f;
-    }
-    currentTime = newTime;
-    accumulator += frametime;
-    while (accumulator >= dt) {
+    while (accumulator > dt) {
+      accumulator -= dt;
       this->_data->machine.getActiveState()->handleInput();
       this->_data->machine.getActiveState()->update(dt);
-      accumulator -= dt;
     }
 
-    interpolation = accumulator / dt;
-    this->_data->machine.getActiveState()->draw(interpolation);
+    frametime = this->_clock.restart().asSeconds();
+    accumulator += frametime;
+    // printf("FPS: %d\n", (int)(1.0f / frametime));
+    this->_data->machine.getActiveState()->draw();
   }
 }
 } // namespace Skeleton
