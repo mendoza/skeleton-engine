dc37e25d7d2420cc8602f55fa8c37a45bea2d9ad
David Mendoza <d.mendoza99@unitec.edu>
2020-05-09 13:14:09 -0600

    removed components and systems because its a framework

--- include/Components.hpp
+++ include/Components.hpp
@@ -1,80 +1,9 @@
 #pragma once
-#include <Game.hpp>
-#include <LuaHelper.hpp>
-#include <SFML/Graphics.hpp>
-#include <sol.hpp>
-namespace Skeleton {
+#include <Engine.hpp>
+
 class Component {
-public:
-  virtual void update(float dt){};
-  virtual void draw(){};
-  virtual ~Component(){};
+  public:
+	virtual void update(float dt){};
+	virtual void draw(){};
+	virtual ~Component(){};
 };
-class GraphicComponent : public Component {
-public:
-  sf::Sprite sprite;
-  sf::Vector2u imageCount;
-  sf::Vector2u currentImage;
-  std::string name;
-  sf::IntRect uvRect;
-  float switchTime;
-  float totalTime = 0.0f;
-  bool animated = false;
-  int row = 0;
-
-  GraphicComponent(gameDataRef data, sol::table gc) {
-    this->name = gc["spriteName"];
-    data->assets.loadTexture(this->name, gc["spriteFilepath"]);
-    sf::Texture &text = data->assets.getTexture(this->name);
-    sf::Vector2f scale(gc["scale"]["width"], gc["scale"]["height"]);
-    if (gc["animation"] != sol::nil) {
-      this->totalTime = 0.0f;
-      this->animated = true;
-      this->imageCount = sf::Vector2u(gc["animation"]["horizontalFrameCount"],
-                                      gc["animation"]["verticalFrameCount"]);
-      this->uvRect.width = text.getSize().x / float(this->imageCount.x);
-      this->uvRect.height = text.getSize().y / float(this->imageCount.y);
-      this->row = gc["animation"]["row"];
-      this->switchTime = gc["animation"]["switchTime"];
-      this->currentImage.x = 0;
-      this->currentImage.y = this->row;
-      this->sprite.setTextureRect(this->uvRect);
-    }
-    this->sprite.setTexture(text);
-    this->sprite.setScale(scale);
-  }
-
-  void update(float dt) override {
-    if (this->animated) {
-      this->totalTime += dt;
-      if (totalTime >= switchTime) {
-        totalTime -= switchTime;
-        currentImage.x++;
-        if (currentImage.x >= imageCount.x) {
-          currentImage.x = 0;
-        }
-      }
-      uvRect.left = currentImage.x * uvRect.width;
-      uvRect.top = currentImage.y * uvRect.height;
-      this->sprite.setTextureRect(this->uvRect);
-    }
-  }
-};
-
-class movableComponent : public Component {
-  sf::Vector2i velocity;
-  sf::Vector2f position;
-  int speed;
-
-  movableComponent() {
-    this->speed = 10;
-    this->velocity = sf::Vector2i(0, 0);
-    this->position = sf::Vector2f(10, 10);
-  }
-
-  void update(float dt) override {
-    position.x += static_cast<int>(velocity.x * speed);
-    position.y += static_cast<int>(velocity.y * speed);
-  }
-};
-} // namespace Skeleton
--- include/Systems.hpp
+++ include/Systems.hpp
@@ -1,31 +1,3 @@
 #pragma once
 #include <Components.hpp>
-#include <ECS.hpp>
-#include <Game.hpp>
-
-namespace Skeleton {
-class drawGraphicSystem : public System {
-public:
-  gameDataRef data;
-  drawGraphicSystem(gameDataRef data) { this->data = data; }
-  void update(float time) override {
-    // Get the entity manager using entities() function
-    for (auto entity : entities().with<GraphicComponent>()) {
-      entity.get<GraphicComponent>().update(time);
-      this->data->window.draw(entity.get<GraphicComponent>().sprite);
-    }
-  }
-};
-
-class movingGraphicSystem : public System {
-public:
-  gameDataRef data;
-  movingGraphicSystem(gameDataRef data) { this->data = data; }
-  void update(float time) override {
-    // Get the entity manager using entities() function
-    for (auto entity : entities().with<GraphicComponent, movableComponent>()) {
-      entity.get<GraphicComponent>().sprite.setPosition(10, 10);
-    }
-  }
-};
-} // namespace Skeleton
+#include <Engine.hpp>
