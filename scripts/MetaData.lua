0d81365873c9a7c5f6ed2a085a3a58b1e7d10060
David Mendoza <d.mendoza99@unitec.edu>
2020-05-09 13:06:19 -0600

    I changed the metadata file for a more structed one

--- scripts/MetaData.lua
+++ scripts/MetaData.lua
@@ -1,16 +1,18 @@
-metaData = {
-    title = "Skeleton Engine",
+MetaData = {
+    title = "Learn Tanks",
     width = 800,
     height = 600,
     fpsLimit = 60,
+    vSync = true,
+    debugMode = true,
     icon = "assets/icon.png",
     splash = {
         backgroundImageFile = "assets/splash.png",
         time = 0.5,
         background = {
-            r=255,
-            g=255,
-            b=255
+            r = 255,
+            g = 255,
+            b = 255
         }
     }
 }
