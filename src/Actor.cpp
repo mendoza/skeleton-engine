f416eb04a23532d855809d196b89a05f64268369
David Mendoza <david.mendoza@herounit.io>
2021-10-09 19:59:45 -0600

    fixed forward and backward functions for the scripting

--- src/Actor.cpp
+++ src/Actor.cpp
@@ -17,13 +17,22 @@ Actor::~Actor() {}
 
 void Actor::rotate(float Angle) {
 	this->get<GraphicComponent>().Sprite.rotate(Angle);
+	this->get<GraphicComponent>().SpriteRotation += Angle;
 }
 
 void Actor::forward() {
 	float speed = this->get<LogicComponent>().L["physics"]["speed"];
+	float angle = this->get<GraphicComponent>().SpriteRotation * (M_PI / 180.f);
+	sf::Vector2f direction = {cos(-angle), sin(-angle)};
+	direction *= speed;
+	this->get<GraphicComponent>().Sprite.move(direction);
 }
 
 void Actor::backward() {
 	float speed = this->get<LogicComponent>().L["physics"]["speed"];
+	float angle = this->get<GraphicComponent>().SpriteRotation * (M_PI / 180.f);
+	sf::Vector2f direction = {cos(-angle), sin(-angle)};
+	direction *= -speed;
+	this->get<GraphicComponent>().Sprite.move(direction);
 }
 void Actor::stop() {}
